21a5adc257eb20e903302495ab4ab3272d6e6a96
Ameet Rahane <ameetrahane@gmail.com>
2021-10-26 11:37:51 -0700

    added replay memory

--- networks/DQN.py
+++ networks/DQN.py
@@ -4,6 +4,12 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torchvision.transforms as T
 
+from collections import namedtuple
+import random
+
+
+Transition = namedtuple('Transition',  ('state', 'action', 'next_state', 'reward'))
+
 class DQN(nn.Module):
     """
     This class implements DQN with conv layers. The idea is to take an impage of the Pong
@@ -36,3 +42,27 @@ class DQN(nn.Module):
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
+
+
+
+class ReplayMemory:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+
+    def push(self, *args):
+        """Saves a transition."""
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+        
+
+
